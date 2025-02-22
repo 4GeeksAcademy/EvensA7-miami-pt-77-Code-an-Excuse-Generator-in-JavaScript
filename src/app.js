fbc42c363f0d8656f630d75ac05c6fe49f1a6978
Evens Antoine <evens7antoine@gmail.com>
2025-02-22 02:17:11 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["my 40 feet tall Kangaroo ", "my dad ", "my left toenail ", "my grandpa "]
+  let what = ["threw away ", "burned up ", "gifted away ", "ran with "]
+  let posession = ["my PS5 ", "my hairline ", "my Jordans ", "my glasses "]
+  let when = ["yesterday", "last night", "2 weeks ago", "an hour ago"]
+
+  let whoIndex = Math.floor(Math.random() * who.length)
+  let whatIndex = Math.floor(Math.random() * what.length)
+  let posessionIndex = Math.floor(Math.random() * posession.length)
+  let whenIndex = Math.floor(Math.random() * when.length)
+
+console.log(whoIndex);
+let docCenterPage = document.querySelector(".center-page")
+docCenterPage.innerHTML = who[whoIndex] + what[whatIndex] + posession[posessionIndex] + when[whenIndex]
 };
