fd7924490b3e4cdc4606651388bba06ecd4479f3
Evens Antoine <evens7antoine@gmail.com>
2025-02-25 01:26:41 +0000

    Project Revisions

--- src/app.js
+++ src/app.js
@@ -5,18 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   let who = ["my 40 feet tall Kangaroo ", "my dad ", "my left toenail ", "my grandpa "]
   let what = ["threw away ", "burned up ", "gifted away ", "ran with "]
-  let posession = ["my PS5 ", "my hairline ", "my Jordans ", "my glasses "]
+  let item = ["my PS5 ", "my hairline ", "my Jordans ", "my glasses "]
   let when = ["yesterday", "last night", "2 weeks ago", "an hour ago"]
 
   let whoIndex = Math.floor(Math.random() * who.length)
   let whatIndex = Math.floor(Math.random() * what.length)
-  let posessionIndex = Math.floor(Math.random() * posession.length)
+  let itemIndex = Math.floor(Math.random() * item.length)
   let whenIndex = Math.floor(Math.random() * when.length)
 
-console.log(whoIndex);
-let docCenterPage = document.querySelector(".center-page")
-docCenterPage.innerHTML = who[whoIndex] + what[whatIndex] + posession[posessionIndex] + when[whenIndex]
+  console.log(whoIndex);
+  let docCenterPage = document.querySelector(".center-page")
+  docCenterPage.innerHTML = who[whoIndex] + what[whatIndex] + item[itemIndex] + when[whenIndex]
 };
